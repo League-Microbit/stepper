2c1ee0093675fcdab06bf9863b74d2a13cdcfc59
Eric Busboom <eric@civicknowledge.com>
2025-06-29 12:19:27 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let motor = stepperMotor.createMotor(
+DigitalPin.P13,
+DigitalPin.P14,
+DigitalPin.P15,
+DigitalPin.P16
+)
+motor.setDelay(0)
+basic.forever(function () {
+    basic.showArrow(ArrowNames.East)
+    motor.moveClockwise(0, stepUnit.Steps)
+    basic.showArrow(ArrowNames.West)
+    motor.moveClockwise(0, stepUnit.Steps)
+})
